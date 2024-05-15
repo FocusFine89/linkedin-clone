5f48a9fe15be9b41552846862cc1514d2c0744b1
FocusFine89 <niivanov02@gmail.com>
2024-05-15 10:12:39 +0200

    change store

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,9 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { combineReducers } from "redux";
 import sidebarReducer from "../reducers/sidebarReducer";
+import getUSerReducer from "../reducers/getUserReducer";
+import getExperienceReducer from "../reducers/getExperienceReducer";
 
 const rootReducer = combineReducers({
   sidebar: sidebarReducer,
+  userMe: getUSerReducer,
+  experienceList: getExperienceReducer,
 });
 
 const store = configureStore({
