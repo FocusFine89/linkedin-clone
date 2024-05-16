d1b22c23117acbc04b047d62e834d114958d6ea2
FocusFine89 <niivanov02@gmail.com>
2024-05-16 12:18:39 +0200

    change app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './App.css'
-import './Style.css'
-import './Main.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import "./Style.css";
+import "./Main.css";
 
-import Main from './Components/Main'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import FooterPart from './Components/FooterPart'
-import MyNavBar from './Components/MyNavBar'
-import EsperienzePage from './Components/EsperienzePage'
-import HomePage from './Components/Hompage'
+import Main from "./Components/Main";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import FooterPart from "./Components/FooterPart";
+import MyNavBar from "./Components/MyNavBar";
+import EsperienzePage from "./Components/EsperienzePage";
+import HomePage from "./Components/Hompage";
 // import SideBarHome from "./Components/SideBarHome";
 // import Post from "./Components/Post";
-import ModificaEsperienze from './Components/ModificaEsperienze'
+import ModificaEsperienze from "./Components/ModificaEsperienze";
 
 function App() {
   return (
@@ -21,9 +21,9 @@ function App() {
         {/* <SideBarHome /> */}
         {/* <Post /> */}
         <Routes>
-          <Route path="/home" element={<HomePage />} />
+          <Route path="/" element={<HomePage />} />
 
-          <Route path="/" element={<Main />} />
+          <Route path="/Profile" element={<Main />} />
           <Route path="/experience/:userID" element={<EsperienzePage />} />
           <Route
             path="/modifica/:userID/:expID"
@@ -33,7 +33,7 @@ function App() {
         <FooterPart />
       </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
