5d8eab51c4af266d7f95c724ac14dc3d864a7c4d
Yahia Lakrikba <161433620+YahiaLakrikba2004@users.noreply.github.com>
2024-05-16 12:12:42 +0200

    pulizia e modifiche

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
-import "./Style.css";
-import "./Main.css";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+import './Style.css'
+import './Main.css'
 
-import Main from "./Components/Main";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import FooterPart from "./Components/FooterPart";
-import MyNavBar from "./Components/MyNavBar";
-import EsperienzePage from "./Components/EsperienzePage";
+import Main from './Components/Main'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import FooterPart from './Components/FooterPart'
+import MyNavBar from './Components/MyNavBar'
+import EsperienzePage from './Components/EsperienzePage'
+import HomePage from './Components/Hompage'
 // import SideBarHome from "./Components/SideBarHome";
 // import Post from "./Components/Post";
-import ModificaEsperienze from "./Components/ModificaEsperienze";
+import ModificaEsperienze from './Components/ModificaEsperienze'
 
 function App() {
   return (
@@ -20,6 +21,8 @@ function App() {
         {/* <SideBarHome /> */}
         {/* <Post /> */}
         <Routes>
+          <Route path="/home" element={<HomePage />} />
+
           <Route path="/" element={<Main />} />
           <Route path="/experience/:userID" element={<EsperienzePage />} />
           <Route
@@ -30,7 +33,7 @@ function App() {
         <FooterPart />
       </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
