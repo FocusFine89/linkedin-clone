c1347b761843ccc9c9a7e2cc4f196f9316289af3
Yahia Lakrikba <161433620+YahiaLakrikba2004@users.noreply.github.com>
2024-05-16 16:33:50 +0200

    modifiche jobs side bar, jobs page e navbar

--- src/Components/MyNavBar.jsx
+++ src/Components/MyNavBar.jsx
@@ -1,171 +1,166 @@
-import { Col, Container, Form, Nav, Navbar } from "react-bootstrap";
-import "../MyNavBar.css";
-import { Link, useLocation } from "react-router-dom";
+import { Col, Container, Form, Nav, Navbar } from 'react-bootstrap'
+import '../MyNavBar.css'
+import { Link, NavLink } from 'react-router-dom'
+
 const MyNavBar = () => {
-  const location = useLocation();
-
   return (
-    <>
-      {" "}
-      <Navbar bg="white" className="NavbarCustom">
-        <Container className=" d-flex align-items-center">
-          <Col xs={1} className="text-end">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              data-supported-dps="24x24"
-              fill="blue"
-              className="mercado-match"
-              width="41"
-              height="41"
-              focusable="false"
-            >
-              <path d="M20.5 2h-17A1.5 1.5 0 002 3.5v17A1.5 1.5 0 003.5 22h17a1.5 1.5 0 001.5-1.5v-17A1.5 1.5 0 0020.5 2zM8 19H5v-9h3zM6.5 8.25A1.75 1.75 0 118.3 6.5a1.78 1.78 0 01-1.8 1.75zM19 19h-3v-4.74c0-1.42-.6-1.93-1.38-1.93A1.74 1.74 0 0013 14.19a.66.66 0 000 .14V19h-3v-9h2.9v1.3a3.11 3.11 0 012.7-1.4c1.55 0 3.36.86 3.36 3.66z"></path>
-            </svg>
-          </Col>
-          <Col xs={3}>
-            <Form.Control
-              className="SearchBar w-75 d-none d-lg-block"
-              placeholder="Stefano Miceli"
-              aria-label="Username"
-            />
-          </Col>
-          <Col xs={8}>
-            <Nav className="align-items-center mx-3">
-              <div className="iconNavBar me-2">
-                <Link
-                  className={`navbarText ${
-                    location.pathname === "/" ? "active" : ""
-                  }`}
-                  to="/home"
+    <Navbar bg="white" className="NavbarCustom">
+      <Container className="d-flex align-items-center">
+        <Col xs={1} className="text-end">
+          {/* Aggiungi qui il tuo logo */}
+        </Col>
+        <Col xs={3}>
+          <Form.Control
+            className="SearchBar w-75 d-none d-lg-block"
+            placeholder="Stefano Miceli"
+            aria-label="Username"
+          />
+        </Col>
+        <Col xs={8}>
+          <Nav className="align-items-center mx-3">
+            <div className="iconNavBar me-2">
+              <NavLink
+                className="navbarText"
+                to="/"
+                activeClassName="active"
+                exact
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  data-supported-dps="24x24"
+                  fill="rgb(102,102,102)"
+                  className="mercado-match "
+                  width="27"
+                  height="30"
+                  focusable="false"
                 >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    data-supported-dps="24x24"
-                    fill="rgb(102,102,102)"
-                    className="mercado-match "
-                    width="27"
-                    height="30"
-                    focusable="false"
-                  >
-                    <path d="M23 9v2h-2v7a3 3 0 01-3 3h-4v-6h-4v6H6a3 3 0 01-3-3v-7H1V9l11-7z"></path>
-                  </svg>
-                  Home
-                </Link>
-              </div>
-              <div className="iconNavBar">
-                <Link className="navbarText pt-0" href="#features">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    data-supported-dps="24x24"
-                    fill="rgb(102,102,102)"
-                    className="mercado-match"
-                    width="27"
-                    height="30"
-                    focusable="false"
-                  >
-                    <path d="M12 16v6H3v-6a3 3 0 013-3h3a3 3 0 013 3zm5.5-3A3.5 3.5 0 1014 9.5a3.5 3.5 0 003.5 3.5zm1 2h-2a2.5 2.5 0 00-2.5 2.5V22h7v-4.5a2.5 2.5 0 00-2.5-2.5zM7.5 2A4.5 4.5 0 1012 6.5 4.49 4.49 0 007.5 2z"></path>
-                  </svg>
-                  Rete
-                </Link>
-              </div>
-              <div className="iconNavBar">
-                <Link className="navbarText pt-0 px-4" to="/jobs">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    data-supported-dps="24x24"
-                    fill="rgb(102,102,102)"
-                    className="mercado-match"
-                    width="27"
-                    height="30"
-                    focusable="false"
-                  >
-                    <path d="M17 6V5a3 3 0 00-3-3h-4a3 3 0 00-3 3v1H2v4a3 3 0 003 3h14a3 3 0 003-3V6zM9 5a1 1 0 011-1h4a1 1 0 011 1v1H9zm10 9a4 4 0 003-1.38V17a3 3 0 01-3 3H5a3 3 0 01-3-3v-4.38A4 4 0 005 14z"></path>
-                  </svg>
-                  Lavoro
-                </Link>
-              </div>
-              <div className="iconNavBar">
-                <Link
-                  className="navbarText pt-0 "
-                  href="#pricing"
-                  activeClassName="active"
+                  <path d="M23 9v2h-2v7a3 3 0 01-3 3h-4v-6h-4v6H6a3 3 0 01-3-3v-7H1V9l11-7z"></path>
+                </svg>
+                Home
+              </NavLink>
+            </div>
+            <div className="iconNavBar">
+              <NavLink
+                className="navbarText pt-0"
+                to="/network"
+                activeClassName="active"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  data-supported-dps="24x24"
+                  fill="rgb(102,102,102)"
+                  className="mercado-match"
+                  width="27"
+                  height="30"
+                  focusable="false"
                 >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    data-supported-dps="24x24"
-                    fill="rgb(102,102,102)"
-                    className="mercado-match"
-                    width="27"
-                    height="30"
-                    focusable="false"
-                  >
-                    <path d="M16 4H8a7 7 0 000 14h4v4l8.16-5.39A6.78 6.78 0 0023 11a7 7 0 00-7-7zm-8 8.25A1.25 1.25 0 119.25 11 1.25 1.25 0 018 12.25zm4 0A1.25 1.25 0 1113.25 11 1.25 1.25 0 0112 12.25zm4 0A1.25 1.25 0 1117.25 11 1.25 1.25 0 0116 12.25z"></path>
-                  </svg>{" "}
-                  Messaggistica
-                </Link>
-              </div>
-              <div className="iconNavBar">
-                <Link className="navbarText pt-0" href="#pricing">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    data-supported-dps="24x24"
-                    fill="rgb(102,102,102)"
-                    className="mercado-match"
-                    width="27"
-                    height="30"
-                    focusable="false"
-                  >
-                    <path d="M22 19h-8.28a2 2 0 11-3.44 0H2v-1a4.52 4.52 0 011.17-2.83l1-1.17h15.7l1 1.17A4.42 4.42 0 0122 18zM18.21 7.44A6.27 6.27 0 0012 2a6.27 6.27 0 00-6.21 5.44L5 13h14z"></path>
-                  </svg>{" "}
-                  Notifiche
-                </Link>
-              </div>
-              <div className="iconNavBar">
-                <Link
-                  className={`navbarText ${
-                    location.pathname === "/" ? "active" : ""
-                  }`}
-                  to="/"
+                  <path d="M12 16v6H3v-6a3 3 0 013-3h3a3 3 0 013 3zm5.5-3A3.5 3.5 0 1014 9.5a3.5 3.5 0 003.5 3.5zm1 2h-2a2.5 2.5 0 00-2.5 2.5V22h7v-4.5a2.5 2.5 0 00-2.5-2.5zM7.5 2A4.5 4.5 0 1012 6.5 4.49 4.49 0 007.5 2z"></path>
+                </svg>
+                Rete
+              </NavLink>
+            </div>
+            <div className="iconNavBar">
+              <NavLink
+                className="navbarText pt-0"
+                to="/jobs"
+                activeClassName="active"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  data-supported-dps="24x24"
+                  fill="rgb(102,102,102)"
+                  className="mercado-match"
+                  width="27"
+                  height="30"
+                  focusable="false"
                 >
-                  <img
-                    src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png"
-                    alt=""
-                    className="avatarStyle rounded-circle"
-                  />
-                  Tu
-                </Link>
-              </div>
+                  <path d="M17 6V5a3 3 0 00-3-3h-4a3 3 0 00-3 3v1H2v4a3 3 0 003 3h14a3 3 0 003-3V6zM9 5a1 1 0 011-1h4a1 1 0 011 1v1H9zm10 9a4 4 0 003-1.38V17a3 3 0 01-3 3H5a3 3 0 01-3-3v-4.38A4 4 0 005 14z"></path>
+                </svg>
+                Lavoro
+              </NavLink>
+            </div>
+            <div className="iconNavBar">
+              <NavLink
+                className="navbarText pt-0"
+                to="/messaging"
+                activeClassName="active"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  data-supported-dps="24x24"
+                  fill="rgb(102,102,102)"
+                  className="mercado-match"
+                  width="27"
+                  height="30"
+                  focusable="false"
+                >
+                  <path d="M16 4H8a7 7 0 000 14h4v4l8.16-5.39A6.78 6.78 0 0023 11a7 7 0 00-7-7zm-8 8.25A1.25 1.25 0 119.25 11 1.25 1.25 0 018 12.25zm4 0A1.25 1.25 0 1113.25 11 1.25 1.25 0 0112 12.25zm4 0A1.25 1.25 0 1117.25 11 1.25 1.25 0 0116 12.25z"></path>
+                </svg>{' '}
+                Messaggistica
+              </NavLink>
+            </div>
+            <div className="iconNavBar">
+              <NavLink
+                className="navbarText pt-0"
+                to="/notifications"
+                activeClassName="active"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  data-supported-dps="24x24"
+                  fill="rgb(102,102,102)"
+                  className="mercado-match"
+                  width="27"
+                  height="30"
+                  focusable="false"
+                >
+                  <path d="M22 19h-8.28a2 2 0 11-3.44 0H2v-1a4.52 4.52 0 011.17-2.83l1-1.17h15.7l1 1.17A4.42 4.42 0 0122 18zM18.21 7.44A6.27 6.27 0 0012 2a6.27 6.27 0 00-6.21 5.44L5 13h14z"></path>
+                </svg>{' '}
+                Notifiche
+              </NavLink>
+            </div>
+            <div className="iconNavBar">
+              <NavLink
+                className="navbarText pt-0"
+                to="/profile"
+                activeClassName="active"
+              >
+                <img
+                  src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png"
+                  alt=""
+                  className="avatarStyle rounded-circle"
+                />
+                Tu
+              </NavLink>
+            </div>
+            <div className="iconNavBar">
+              <Link className="navbarText pt-0 ms-3">
+                {' '}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  data-supported-dps="24x24"
+                  fill="rgb(102,102,102)"
+                  className="mercado-match"
+                  width="27"
+                  height="30"
+                  focusable="false"
+                >
+                  <path d="M3 3h4v4H3zm7 4h4V3h-4zm7-4v4h4V3zM3 14h4v-4H3zm7 0h4v-4h-4zm7 0h4v-4h-4zM3 21h4v-4H3zm7 0h4v-4h-4zm7 0h4v-4h-4z"></path>
+                </svg>{' '}
+                Per aziende
+              </Link>
+            </div>
+          </Nav>
+        </Col>
+      </Container>
+    </Navbar>
+  )
+}
 
-              <div className="iconNavBar">
-                <Link className="navbarText pt-0">
-                  {" "}
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    data-supported-dps="24x24"
-                    fill="rgb(102,102,102)"
-                    className="mercado-match"
-                    width="27"
-                    height="30"
-                    focusable="false"
-                  >
-                    <path d="M3 3h4v4H3zm7 4h4V3h-4zm7-4v4h4V3zM3 14h4v-4H3zm7 0h4v-4h-4zm7 0h4v-4h-4zM3 21h4v-4H3zm7 0h4v-4h-4zm7 0h4v-4h-4z"></path>
-                  </svg>{" "}
-                  Per aziende
-                </Link>
-              </div>
-            </Nav>
-          </Col>
-        </Container>
-      </Navbar>
-    </>
-  );
-};
-
-export default MyNavBar;
+export default MyNavBar
