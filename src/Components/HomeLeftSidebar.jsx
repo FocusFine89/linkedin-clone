d53dfc9c22ffd74bc1aaf1c846a0f824766b0ad5
FocusFine89 <niivanov02@gmail.com>
2024-05-17 16:38:22 +0200

    small changes

--- src/Components/HomeLeftSidebar.jsx
+++ src/Components/HomeLeftSidebar.jsx
@@ -1,9 +1,9 @@
-import { Button, Card, Col, Image, ListGroup } from 'react-bootstrap'
-import { useSelector } from 'react-redux'
-import { BookmarkFill, Plus } from 'react-bootstrap-icons'
-import MostraAnalisi from './MostraAnalisi'
+import { Button, Card, Col, Image, ListGroup } from "react-bootstrap";
+import { useSelector } from "react-redux";
+import { BookmarkFill, Plus } from "react-bootstrap-icons";
+import MostraAnalisi from "./MostraAnalisi";
 const HomeLeftSideBar = () => {
-  const user = useSelector(state => state.userMe.content)
+  const user = useSelector((state) => state.userMe.content);
 
   return (
     <>
@@ -18,12 +18,12 @@ const HomeLeftSideBar = () => {
               <Image
                 className="rounded-circle cirleImgStyle position-absolute"
                 src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png"
-              />{' '}
+              />{" "}
             </div>
             <Card.Body className="mt-5">
               <Card.Title>Ti diamo il benvenuto {user.name}</Card.Title>
               <Card.Link
-                className="link-offset-2 link-underline link-underline-opacity-0"
+                className="link-offset-2 link-underline link-underline-opacity-0 text-primary"
                 href="#"
               >
                 Aggiungi una foto
@@ -66,7 +66,7 @@ const HomeLeftSideBar = () => {
             <Card.Text>
               <Card.Link
                 href="#"
-                className="link-offset-2 link-underline link-underline-opacity-0"
+                className="link-offset-2 link-underline link-underline-opacity-0 text-primary"
               >
                 Gruppi
               </Card.Link>
@@ -74,21 +74,21 @@ const HomeLeftSideBar = () => {
             <Card.Text className="d-flex justify-content-between align-items-center ">
               <Card.Link
                 href="#"
-                className="link-offset-2 link-underline link-underline-opacity-0"
+                className="link-offset-2 link-underline link-underline-opacity-0 text-primary"
               >
-                Eventi{' '}
+                Eventi{" "}
               </Card.Link>
               <Button type="button" className="p-0 bg-transparent border-0">
-                {' '}
+                {" "}
                 <Plus size={30} color="black" />
               </Button>
             </Card.Text>
             <Card.Text>
               <Card.Link
                 href="#"
-                className="link-offset-2 link-underline link-underline-opacity-0"
+                className="link-offset-2 link-underline link-underline-opacity-0 text-primary"
               >
-                Hashtag seguiti{' '}
+                Hashtag seguiti{" "}
               </Card.Link>
             </Card.Text>
           </Card.Body>
@@ -96,7 +96,7 @@ const HomeLeftSideBar = () => {
         </Card>
       </Col>
     </>
-  )
-}
+  );
+};
 
-export default HomeLeftSideBar
+export default HomeLeftSideBar;
