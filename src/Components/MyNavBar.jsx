2f4d0a3089ed3ac43c801aa9006ea07e4bafca25
FocusFine89 <niivanov02@gmail.com>
2024-05-17 15:25:06 +0200

    responsive

--- src/Components/MyNavBar.jsx
+++ src/Components/MyNavBar.jsx
@@ -1,11 +1,11 @@
-import { Col, Container, Form, Nav, Navbar } from 'react-bootstrap'
-import '../MyNavBar.css'
-import { Link, NavLink } from 'react-router-dom'
+import { Col, Container, Form, Nav, Navbar } from "react-bootstrap";
+import "../MyNavBar.css";
+import { Link, NavLink } from "react-router-dom";
 
 const MyNavBar = () => {
   return (
-    <Navbar bg="white" className="NavbarCustom">
-      <Container className="d-flex align-items-center ">
+    <Navbar bg="white" className="NavbarCustom ">
+      <Container fluid className="d-flex align-items-center ">
         <Col xs={1} className="text-end"></Col>
         <Col xs={3} className="d-flex">
           <img
@@ -64,6 +64,7 @@ const MyNavBar = () => {
                 Rete
               </NavLink>
             </div>
+
             <div className="iconNavBar">
               <NavLink
                 className="navbarText pt-0"
@@ -102,7 +103,7 @@ const MyNavBar = () => {
                   focusable="false"
                 >
                   <path d="M16 4H8a7 7 0 000 14h4v4l8.16-5.39A6.78 6.78 0 0023 11a7 7 0 00-7-7zm-8 8.25A1.25 1.25 0 119.25 11 1.25 1.25 0 018 12.25zm4 0A1.25 1.25 0 1113.25 11 1.25 1.25 0 0112 12.25zm4 0A1.25 1.25 0 1117.25 11 1.25 1.25 0 0116 12.25z"></path>
-                </svg>{' '}
+                </svg>{" "}
                 Messaggistica
               </NavLink>
             </div>
@@ -123,7 +124,7 @@ const MyNavBar = () => {
                   focusable="false"
                 >
                   <path d="M22 19h-8.28a2 2 0 11-3.44 0H2v-1a4.52 4.52 0 011.17-2.83l1-1.17h15.7l1 1.17A4.42 4.42 0 0122 18zM18.21 7.44A6.27 6.27 0 0012 2a6.27 6.27 0 00-6.21 5.44L5 13h14z"></path>
-                </svg>{' '}
+                </svg>{" "}
                 Notifiche
               </NavLink>
             </div>
@@ -143,7 +144,7 @@ const MyNavBar = () => {
             </div>
             <div className="  Aziende-part iconNavBar">
               <Link className="navbarText pt-0 ms-3">
-                {' '}
+                {" "}
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 24 24"
@@ -155,7 +156,7 @@ const MyNavBar = () => {
                   focusable="false"
                 >
                   <path d="M3 3h4v4H3zm7 4h4V3h-4zm7-4v4h4V3zM3 14h4v-4H3zm7 0h4v-4h-4zm7 0h4v-4h-4zM3 21h4v-4H3zm7 0h4v-4h-4zm7 0h4v-4h-4z"></path>
-                </svg>{' '}
+                </svg>{" "}
                 Per aziende
               </Link>
             </div>
@@ -163,7 +164,7 @@ const MyNavBar = () => {
         </Col>
       </Container>
     </Navbar>
-  )
-}
+  );
+};
 
-export default MyNavBar
+export default MyNavBar;
